d9e09280bfc4fb2112a572e14207dbb0e975f026
Jasha Puzon <jpuzon@ucsc.edu>
2022-06-02 23:20:17 -0700

    menusheet and other shiz

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,10 +4,31 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-
+      this.load.spritesheet('menu-sheet', './assets/menusheet.png', {
+        frameWidth: 1602, // lmao what is this res
+        frameHeight: 890,
+        startFrame: 0,
+        endFrame: 6
+      });
     }
 
     create() {
+
+      this.anims.create({
+        key: 'menu',
+        frames: this.anims.generateFrameNames('menu-sheet', {start: 0, end: 7}),
+        frameRate: 5, // what was this?
+        loop: true,
+        //yoyo: true,
+        repeat: -1,
+      });
+
+      let background = this.add.sprite(0, game.config.height / 2, 'menu', 0).setOrigin(0, 0.5);
+      background.anims.play('menu');        
+      
+      background.displayWidth = game.config.width;
+      background.displayHeight = background.displayWidth * background.height / background.width;
+
         //this.keyEnter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
         let textConfig = {
           fontFamily: 'Courier',
